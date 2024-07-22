e73969e58713b46679737037253d4a434f10612d
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-22 16:57:31 -0300

    chore: Add .env.example, jest.config.js, .prettierrc, extensions.json, ecosystem.config.js, .eslintrc.cjs, and knexfile.js files

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true, 
+    browser: false 
+  },
+  extends: [
+    'eslint:recommended',
+    'google', 
+    'plugin:prettier/recommended' 
+  ],
+  parserOptions: {
+    ecmaVersion: 12, 
+    sourceType: 'module'
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  plugins: [
+    
+  ],
+  rules: {
+    'require-jsdoc': 'off',
+    'indent': ['error', 2],
+    'object-curly-spacing': ['error', 'always'],
+    'max-len': 'off',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }]
+  },
+};
